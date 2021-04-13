a3a4353d1fe10715c8fbdd3bd7269a0a5a98324d
Pakvothe <ortiz.franco@yandex.com>
2021-04-13 02:18:57 -0300

    implemented problem A logic

---
+++ src/LinkedListLogic.js
@@ -0,0 +1,43 @@
+export class Node {
+    constructor(data,next){
+      this.data = data;
+      this.next = next;
+    }
+
+  }
+  
+export class LinkedList {
+    constructor() {
+      this.head = null;
+    }
+  
+    addTail(data) {
+      const newNode = new Node(data, null);
+      if(!this.head){
+        this.head = newNode
+      } else {
+        let current = this.head;
+        while (current.next){
+          current = current.next;
+        };
+        current.next = newNode;
+      };
+    }
+    addHead(head,data) {
+      let newNode = new Node(data,null)
+      if(!this.head) {
+          this.head= newNode
+          return head
+      } else {
+          newNode.next = this.head
+          this.head =newNode
+      }
+      return this.head
+  }
+  
+}
+export let linkedList = new LinkedList();
+
+
+
+console.log(`linkedList`, linkedList)
